fa4cc00d5ac5179678964b36cab34794b11796a1
gcgcode <gnzlcantosies@gmail.com>
2022-08-19 03:41:59 -0400

    init config

--- bot.py
+++ bot.py
@@ -1,1 +1,20 @@
-print("Hello")
+
+import requests
+from bs4 import BeautifulSoup
+ 
+def count_words(url, the_word):
+    r = requests.get(url, allow_redirects=False)
+    soup = BeautifulSoup(r.content, 'lxml')
+    words = soup.find(text=lambda text: text and the_word in text)
+    print(words)
+    return len(words)
+ 
+ 
+def main():
+    url = 'https://python-forum.io/thread-8147.html'
+    word = 'code'
+    count = count_words(url, word)
+    print('\nUrl: {}\ncontains {} occurrences of word: {}'.format(url, count, word))
+ 
+if __name__ == '__main__':
+    main()
