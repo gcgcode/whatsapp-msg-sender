f4b46a76325163882ebaac16645594312b250660
gcgcode <gnzlcantosies@gmail.com>
2022-08-19 06:41:12 -0400

    Main app structure

---
+++ app/whatsapp_client.py
@@ -0,0 +1,38 @@
+import os
+import requests
+
+import json
+
+
+class WhatsAppWrapper:
+
+    API_URL = "https://graph.facebook.com/v13.0/"
+    API_TOKEN = os.environ.get("WHATSAPP_API_TOKEN")
+    NUMBER_ID = os.environ.get("WHATSAPP_NUMBER_ID")
+
+    def __init__(self):
+        self.headers = {
+            "Authorization": f"Bearer {self.API_TOKEN}",
+            "Content-Type": "application/json",
+        }
+        self.API_URL = self.API_URL + self.NUMBER_ID
+
+    def send_template_message(self, template_name, language_code, phone_number):
+
+        payload = json.dumps({
+            "messaging_product": "whatsapp",
+            "to": phone_number,
+            "type": "template",
+            "template": {
+                "name": template_name,
+                "language": {
+                    "code": language_code
+                }
+            }
+        })
+
+        response = requests.request("POST", f"{self.API_URL}/messages", headers=self.headers, data=payload)
+
+        assert response.status_code == 200, "Error sending message"
+
+        return response.status_code
