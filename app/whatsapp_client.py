7a2e22993669b18ade1d7d163b2bdb1571a9b4eb
gcgcode <gnzlcantosies@gmail.com>
2022-08-22 03:17:33 -0400

    Sloving config.py file (not properly defined import loadenv)

--- app/whatsapp_client.py
+++ app/whatsapp_client.py
@@ -1,24 +1,23 @@
-from cmath import log
-import logging
 import os
 import requests
-
+import logging
 import json
-
+import logging
+from dotenv import load_dotenv
+load_dotenv()
 
 class WhatsAppWrapper:
 
     API_URL = "https://graph.facebook.com/v13.0/"
-    API_TOKEN = os.environ.get("WHATSAPP_API_TOKEN")
-    NUMBER_ID = os.environ.get("WHATSAPP_NUMBER_ID")
+    API_TOKEN = os.getenv('WHATSAPP_API_TOKEN')
+    NUMBER_ID = os.environ.get('WHATSAPP_NUMBER_ID')
 
     def __init__(self):
         self.headers = {
-            #"Authorization": f"Bearer {self.API_TOKEN}",
-            "Authorization": "Bearer EAAPZC3q7dV18BABtENvY2NQzxtwcwiGe5T2GYOpLO92tNuZBwUqMsI8Ltp6JtwVSZCajj9J0FgT3GtO8JTT1H8C9pEnAEVTGW0eEEMrdAFEqldd8BYP9AFI53eX564Tv0t8ZCjLRZAPYeME76cg7fDbCCYO3FXdfem51D2RY3BpcZB17DvecIirsmn72SgG8B28bsmeQrVd3WXGjElmZAhw",
+            "Authorization": f"Bearer {self.API_TOKEN}",
             "Content-Type": "application/json",
         }
-        self.API_URL = str(self.API_URL) + str(self.NUMBER_ID)
+        self.API_URL = self.API_URL + str(self.NUMBER_ID)
 
     def send_template_message(self, template_name, language_code, phone_number):
 
@@ -34,13 +33,13 @@ class WhatsAppWrapper:
             }
         })
 
-        #logging.debug('msg');
+        # Setting log file
+        logging.basicConfig(filename="log.txt", level=logging.DEBUG)
+        #logging.info(self.API_URL + "/messages")
+        #logging.info(self.API_TOKEN)  
 
-        #f"{self.API_URL}/messages"
-
-        response = requests.request("POST", "https://graph.facebook.com/v13.0/105769248921601/messages", headers=self.headers, data=payload)
-        #response = requests.request("POST", str(self.API_URL) + str(self.NUMBER_ID), headers=self.headers, data=payload)
-
+        response = requests.request("POST", f"{self.API_URL}/messages", headers=self.headers, data=payload)
+        
         assert response.status_code == 200, {response.status_code}
 
         return response.status_code
